708f6dd84e95b106198d0a22bbd821c1cf33ee98
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-18 11:41:10 +0100

    some explanation and unnecessairy code removeal

--- Warzone Mods Lua Files/Server_AdvanceTurn.lua
+++ Warzone Mods Lua Files/Server_AdvanceTurn.lua
@@ -1,12 +1,33 @@
-numTerritoriesPerPlayer = {};
+-- This is not necessary
+-- numTerritoriesPerPlayer = {};
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-    Index.PlayerID = 0
+--  Also not necessary, you don't use this anywhere and it will throw an error
+--  Index.PlayerID = 0
 	local numTerritoriesPerPlayer = {};
+
+--	ok, some better explanation about this loop
+--	game.Game.PlayingPlayers contains all the player IDs and the player objects of all players still alive in the game
+--	but we don't know which players are playing and what their player ID is, so we use this for loop
+--	This for loop runs over every entry (KEY and VALUE) in the table
+--	for KEY, VALUE in pairs(TABLE) do		is the general form of this loop
+--	Since we only need the playerID (KEY) here we void (_) the VALUE of each entry
+--	The KEY is now stored in playerID here. This variable changes with each entry in the table
 	for playerID, _ in pairs(game.Game.PlayingPlayers) do
-		numTerritoriesPerPlayer[playerID] = 0;
+--		We then can do something with the KEY and VALUE (here only the KEY)
+		numTerritoriesPerPlayer[playerID] = 0;	-- creates an entry in the TABLE numTerritoriesPerPlayer, with as KEY every player ID
+--		print(playerID);
 	end
 
-function Server_AdvanceTurn_End(game, addNewOrder)
-	game.ServerGame.LatestTerritoryStanding.Territories
-	if PlayerID.TerritoryStanding = WL.PlayerID.Neutral then +1 numTerritoriesPerPlayer end
+-- 	Now that you know more about loops, I recommend creating one yourself :)
+--	here are the things you need:
+--		TABLE 	= 	game.ServerGame.LatestTerritoryStanding.Territories
+--		KEY 	=	void 		(We don't need it)
+--		VALUE	=	territoryStanding
+--	Hint: place a print(VALUE) in the loop body 
+
+--  This is the same function as above, so why not combine it :)
+--	function Server_AdvanceTurn_End(game, addNewOrder)
+--	game.ServerGame.LatestTerritoryStanding.Territories
+--	if PlayerID.TerritoryStanding = WL.PlayerID.Neutral then +1 numTerritoriesPerPlayer end
+end
